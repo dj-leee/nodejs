306cf32b5e26119d558c5345c73ed3babf760f31
dj-lee <djocundadiawara@gmail.com>
2026-01-14 17:15:51 +0100

    feat: estructura basica de la calcu, hecha

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // importar de la libreria de operaciones aritmeticas
-import { sumar, restar, multiplicar, dividir } from './operaciones-aritmeticas.mjs';
+import { sumar, restar, multiplicar, dividir } from '../operaciones-aritmeticas.mjs';
 // Ejemplos de uso de las funciones importadas
 console.log("Suma: ", sumar(10, 5)); // 15
 console.log("Resta: ", restar(10, 5)); // 5
